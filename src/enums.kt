816a750500b3297cd360dfc9fa4b36c97e3515f4
spdys <noorii549@yahoo.com>
2025-02-18 19:59:23 +0300

    Finished step 4 creating a val of type UserRole

--- src/enums.kt
+++ src/enums.kt
@@ -1,5 +1,12 @@
 enum class UserRole(val accessLevel: Int) {
     ADMIN(3),
     EDITOR(2),
-    VIEWER(1)
+    VIEWER(1);
+
+    fun canEdit(): Boolean = this.accessLevel <= 2
+}
+
+fun main() {
+    val role: UserRole = UserRole.ADMIN
+    println("Can you edit?: ${role.canEdit()}")
 }
